773c6a0f5463901d9dc45c6c9eaed21411f74c46
Sebiten <sebdevspace@gmail.com>
2023-12-27 19:41:44 -0300

    uptask

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+const app = express();
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`servidor corriendo en el puerto  ${PORT}`);
+});
