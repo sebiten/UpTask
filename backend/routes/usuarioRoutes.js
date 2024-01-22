9b482106c9393ebfc5581c29094390e050fd5c17
Sebiten <sebdevspace@gmail.com>
2024-01-22 12:57:25 -0300

    confirmarcuenta

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
-import {registrar, autenticar} from "../controllers/usuarioController.js";
+import {
+  registrar,
+  autenticar,
+  confirmar,
+} from "../controllers/usuarioController.js";
 
 // Autenticacion, registros y confirmacion de usuarios
 router.post("/", registrar); // crea un nuevo usuario
-router.post("/login", autenticar)
+router.post("/login", autenticar);
+router.get("/confirmar/:token", confirmar);
 
 export default router;
