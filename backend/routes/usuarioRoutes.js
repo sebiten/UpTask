a979ee2cb11296b881332dad7afe22b82981c4a5
Sebiten <sebdevspace@gmail.com>
2024-01-23 12:02:53 -0300

    cambiar password

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -4,11 +4,15 @@ import {
   registrar,
   autenticar,
   confirmar,
+  olvidePassword,
+  comprobarToken,
 } from "../controllers/usuarioController.js";
 
 // Autenticacion, registros y confirmacion de usuarios
 router.post("/", registrar); // crea un nuevo usuario
 router.post("/login", autenticar);
 router.get("/confirmar/:token", confirmar);
+router.post("/olvide-password", olvidePassword);
+router.get("/olvide-password/:token", comprobarToken);
 
 export default router;
