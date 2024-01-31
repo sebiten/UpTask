f762ed7f4ad70d0248b0b01dac0af0e7379614df
Sebiten <sebdevspace@gmail.com>
2024-01-31 20:00:38 -0300

    almost crud in node

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,6 +1,9 @@
 import Proyecto from "../models/Proyecto.js";
 
-const obtenerProyectos = async (req, res) => {};
+const obtenerProyectos = async (req, res) => {
+  const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+  res.json(proyectos);
+};
 
 const nuevoProyecto = async (req, res) => {
   const proyecto = new Proyecto(req.body);
@@ -14,11 +17,69 @@ const nuevoProyecto = async (req, res) => {
   }
 };
 
-const obetenerProyecto = async (req, res) => {};
+const obetenerProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  console.log(proyecto);
 
-const editarProyecto = async (req, res) => {};
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+  // identificar que la persona que desea obtener sea el que lo creo
 
-const eliminarProyecto = async (req, res) => {};
+  if (proyecto.creador.toString() === req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  res.json(proyecto);
+};
+
+const editarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  console.log(proyecto);
+
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+  // identificar que la persona que desea editar sea el que lo creo
+
+  if (proyecto.creador.toString() === req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  console.log(proyecto);
+
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+  // identificar que la persona que desea eliminar sea el que lo creo
+  if (proyecto.creador.toString() === req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(404).json({ msg: error.message });
+  }
+};
 
 const agregarColaborador = async (req, res) => {};
 
