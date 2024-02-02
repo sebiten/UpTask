03d2d747e7d9b8a31cca13650c88e93b41dc36b5
Sebiten <sebdevspace@gmail.com>
2024-02-02 12:47:00 -0300

    tareastart

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -1,4 +1,11 @@
-const agregarTarea = async (req, res) => {};
+import Proyecto from "../models/Proyecto.js";
+
+const agregarTarea = async (req, res) => {
+  const { proyecto } = req.body;
+
+  const existeProyecto = await Proyecto.findById(proyecto);
+  console.log(existeProyecto);
+};
 const obtenerTarea = async (req, res) => {};
 const actualizarTarea = async (req, res) => {};
 const eliminarTarea = async (req, res) => {};
